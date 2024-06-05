fe0904415cd8fd144bb8063e20025274c0a8fff2
Santosh Bandichode <santosh.bandichode@cyberium.info>
2024-06-06 01:02:48 +0530

    Include nofile completion option

--- src/option.h
+++ src/option.h
@@ -536,6 +536,7 @@ EXTERN unsigned	cot_flags;	// flags from 'completeopt'
 #define COT_NOSORT	    0x200   // TRUE: fuzzy match without qsort score
 #define COT_PREINSERT	    0x400   // TRUE: preinsert
 #define COT_NEAREST	    0x800   // TRUE: prioritize matches close to cursor
+#define COT_NOFILE	    0x800   // TRUE: no file name
 
 #define CFC_KEYWORD         0x001
 #define CFC_FILES           0x002
